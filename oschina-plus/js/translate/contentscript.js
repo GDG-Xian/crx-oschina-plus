1f4804324e8780fb13d44edbd4cea1f59315cc0b
greatghoul <greatghoul@gmail.com>
2013-09-15 15:51:42 +0800

    添加保存草稿的按钮

--- oschina-plus/js/translate/contentscript.js
+++ oschina-plus/js/translate/contentscript.js
@@ -1,1 +1,8 @@
-alert('Hi there');
+// 添加暂存翻译的额按钮
+var actionBar = document.querySelector('.Edit .Top .action');
+var btnDraft = document.createElement('a');
+btnDraft.id = 'btn_draft';
+btnDraft.href = 'javascript:void(0);';
+btnDraft.className = 'DRAFT_BUTTON';
+btnDraft.innerHTML = '保存草稿';
+actionBar.appendChild(btnDraft);
