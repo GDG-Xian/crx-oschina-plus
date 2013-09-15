0d9528111c1adb87ace77d6d4c649583b9f77ee0
greatghoul <greatghoul@gmail.com>
2013-09-15 15:58:01 +0800

    起草保存草稿的方法

--- oschina-plus/js/translate/contentscript.js
+++ oschina-plus/js/translate/contentscript.js
@@ -1,3 +1,7 @@
+function doSaveDraft(evt) {
+	var textContent = document.getElementById('ta_p_content');
+}
+
 // 添加暂存翻译的额按钮
 var actionBar = document.querySelector('.Edit .Top .action');
 var btnDraft = document.createElement('a');
@@ -6,3 +10,4 @@ btnDraft.href = 'javascript:void(0);';
 btnDraft.className = 'DRAFT_BUTTON';
 btnDraft.innerHTML = '保存草稿';
 actionBar.appendChild(btnDraft);
+actionBar.addEventListener('click', doSaveDraft, false);
